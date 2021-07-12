98131b9a59202f47e4dc62686c6479b9de0b7775
parvizwpf <30858096+parvizwpf@users.noreply.github.com>
2021-07-12 21:09:36 +0430

    add infra.Data project

--- CleanArch.UI.Mvc/CleanArch.Mvc/Startup.cs
+++ CleanArch.UI.Mvc/CleanArch.Mvc/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArch.Infra.Data.Context;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +32,11 @@ namespace CleanArch.Mvc
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDbContext<MainDbContext>(options=>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
---
+++ Infra.Data/CleanArch.Infra.Data/Context/MainDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
