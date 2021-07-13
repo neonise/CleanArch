28c829897dbdeaf1229f1e58f8083c6adc31249f
parvizwpf <30858096+parvizwpf@users.noreply.github.com>
2021-07-13 19:44:44 +0430

    add ui

---
+++ CleanArch.UI.Mvc/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var data = courseService.GetCourses();
+
+            return View(data);
+        }
+    }
+}
